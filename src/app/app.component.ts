bb281ec507bc219c52474aa014c42403bb60a0d7
Bodor Gergely <bodorgergely91@gmail.com>
2017-01-14 15:47:52 +0100

    Add the click handler, Expose the selected hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,9 +25,10 @@ const HEROES: Hero[] = [
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
