2b8adc4764aff49dca37e95ee25289b528324725
Bodor Gergely <bodorgergely91@gmail.com>
2017-01-14 23:43:34 +0100

    Dashboard Top Heroes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../shared/services/hero.service';
+import { Hero } from '../shared/classes/hero';
 
 @Component({
   selector: 'my-dashboard',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
   }
 
 }
