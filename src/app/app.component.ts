c1f498f1f47207279aae3cc7d2be83a2c46777e1
Bodor Gergely <bodorgergely91@gmail.com>
2017-01-14 22:45:11 +0100

    Create the HeroService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Hero } from './shared/hero';
+import { Hero } from './shared/classes/hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
